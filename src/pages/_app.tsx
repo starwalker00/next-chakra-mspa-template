f0aa5ebbb73d29ed04440c5da6827bbb502d4826
starwalker00 <aaronwalkerup@gmail.com>
2022-04-17 19:59:16 +0200

    add progress bar between pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppPropsWithLayout } from 'src/custom-types/page'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from 'theme'
+import NextNProgress from "nextjs-progressbar";
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <NextNProgress />
       {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
   )
