bfcaefa01f49d188d601ddf38e96a80935d71077
starwalker00 <aaronwalkerup@gmail.com>
2022-04-17 22:32:49 +0200

    add custom progress bar

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,14 +1,34 @@
 import type { NextPageWithLayout } from 'src/custom-types/page'
 import type { ReactElement } from 'react'
 import { Layout, Navbar } from 'src/components/Layout'
-import { Center, Heading } from '@chakra-ui/react'
+import { Center, Heading, keyframes } from '@chakra-ui/react'
+import { Container, Flex, Progress } from '@chakra-ui/react'
 
 const About: NextPageWithLayout = () => {
+
+    const grow = keyframes({
+        "0%": { transform: "scaleX(0.1)" },
+        "100%": { transform: "scaleX(1)" }
+    });
+    const animationGrow = `${grow} 1s infinite alternate`;
+
+    const gradient = `linear-gradient(
+        to right,
+        transparent 0%,
+        blue 50%,
+        transparent 100%
+      )`;
+
     return (
         <>
-            <Center>
-                <Heading>About</Heading>
-            </Center>
+            <Container>
+                <Heading py={8}>About</Heading>
+                <Flex direction="column">
+                    <Flex direction="row" outline="1px solid blue" borderRadius={12} justifyContent="center" width="400px" bg="blackAlpha.800">
+                        <Progress width='200px' animation={animationGrow} size='lg' backgroundColor='red' borderRadius={12} />
+                    </Flex>
+                </Flex>
+            </Container>
         </>
     )
 }
