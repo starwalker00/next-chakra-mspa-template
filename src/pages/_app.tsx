ce4de5c1c8e50eb53f59bf1c156e037c8ce0da5b
starwalker00 <aaronwalkerup@gmail.com>
2022-04-17 20:42:58 +0200

    add appContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,19 @@
-import type { AppPropsWithLayout } from 'src/custom-types/page'
-
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from 'theme'
+import type { AppPropsWithLayout } from 'src/custom-types/page';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from 'theme';
 import NextNProgress from "nextjs-progressbar";
+import { AppContextProvider } from 'src/context/appContext';
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <NextNProgress />
-      {getLayout(<Component {...pageProps} />)}
-    </ChakraProvider>
+    <AppContextProvider>
+      <ChakraProvider resetCSS theme={theme}>
+        <NextNProgress />
+        {getLayout(<Component {...pageProps} />)}
+      </ChakraProvider>
+    </AppContextProvider>
   )
 }
 
